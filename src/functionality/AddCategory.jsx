3f2e796b8994ab4bb0b7eb1b0d8ca97566d2ac7f
avinashh10x <raavaannn@gmail.com>
2024-09-05 00:12:18 +0530

    ficed addCategory component

--- src/functionality/AddCategory.jsx
+++ src/functionality/AddCategory.jsx
@@ -11,62 +11,45 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
-    Select,
     useDisclosure,
-    useToast // Import useToast hook
+    useToast
 } from '@chakra-ui/react';
 import { ProductContext } from '../context/ProductContext';
 import { db } from '../appwrite/appwriteService';
 
-function AddProduct() {
+function AddCategory() {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const { category, fetchProducts, wareHouse, user } = useContext(ProductContext);
-    const [productName, setProductName] = useState('');
-    const [productDescription, setProductDescription] = useState('');
-    const [productCategory, setProductCategory] = useState('');
-    const [productPrice, setProductPrice] = useState('');
-    const [productQuantity, setProductQuantity] = useState('');
-    const [productLocation, setProductLocation] = useState('');
-    const toast = useToast(); // Initialize useToast hook
+    const [categoryName, setCategoryName] = useState('');
+    const { fetchCategories, user } = useContext(ProductContext);
+    const toast = useToast();
 
-    const handleCreateProduct = async (e) => {
-        e.preventDefault(); // Correctly prevent the default form submission
-        if (!productName || !productDescription || !productCategory || !productPrice || !productQuantity || !productLocation) {
+    const handleCreateCategory = async (e) => {
+        e.preventDefault();
+        if (!categoryName) {
             toast({
                 title: 'Error',
-                description: 'All fields are required.',
+                description: 'Category name is required.',
                 status: 'error',
                 duration: 3000,
                 isClosable: true,
                 position: 'top-right',
                 variant: 'left-accent'
             });
-            return; // Exit the function if validation fails
+            return;
         }
 
-        const product = {
-            title: productName,
-            discription: productDescription, // Corrected spelling of 'description'
-            category: productCategory,
-            price: parseFloat(productPrice),
-            quantity: parseInt(productQuantity),
-            location: [productLocation],
+        const data = {
+            name: categoryName,
             userId: user.$id
         };
-        try {
-            const response = await db.products.create(product);
-            fetchProducts();
-            onClose(); // Close the modal after successful creation
-            setProductName('');
-            setProductDescription('');
-            setProductCategory('');
-            setProductPrice('');
-            setProductQuantity('');
-            setProductLocation('');
 
-            // Show success toast notification
+        try {
+            await db.categories.create(data);
+            fetchCategories();
+            onClose();
+            setCategoryName('');
             toast({
-                title: 'Product Created',
+                title: 'Category Created',
                 status: 'success',
                 duration: 3000,
                 isClosable: true,
@@ -74,11 +57,10 @@ function AddProduct() {
                 variant: 'left-accent'
             });
         } catch (error) {
-            console.error('Error creating product:', error);
-
+            console.error('Error creating category:', error);
             toast({
                 title: 'Error',
-                description: 'Failed to create product.',
+                description: 'Failed to create category.',
                 status: 'error',
                 duration: 3000,
                 isClosable: true,
@@ -90,90 +72,31 @@ function AddProduct() {
 
     return (
         <>
-            <Button
-                p={5}
-                onClick={onOpen}
-                variant={'ghost'}
-                color={'white'}
-                fontWeight={'bold'}
-                fontSize={'large'}
-                _hover={{ bg: 'purple.700' }}
-            >
-                Add Product
+            <Button onClick={onOpen} variant="ghost" color="white" fontWeight="bold" fontSize="large" _hover={{ bg: 'purple.700' }}>
+                Add Category
             </Button>
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Add new Product</ModalHeader>
+                    <ModalHeader>Add New Category</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <form onSubmit={handleCreateProduct}>
-                            <FormControl mb={4}>
-                                <FormLabel>Product Name</FormLabel>
+                        <form onSubmit={handleCreateCategory}>
+                            <FormControl>
+                                <FormLabel>Category Name</FormLabel>
                                 <Input
-                                    placeholder='Enter product name'
-                                    type='text'
-                                    value={productName}
-                                    onChange={(e) => setProductName(e.target.value)}
-                                />
-                            </FormControl>
-                            <FormControl mb={4}>
-                                <FormLabel>Description</FormLabel>
-                                <Input
-                                    placeholder='Write a description'
-                                    type='text'
-                                    value={productDescription}
-                                    onChange={(e) => setProductDescription(e.target.value)}
-                                />
-                            </FormControl>
-                            <FormControl mb={4}>
-                                <FormLabel>Category</FormLabel>
-                                <Select
-                                    placeholder='Select Category'
-                                    value={productCategory}
-                                    onChange={(e) => setProductCategory(e.target.value)}
-                                >
-                                    {category.map((cat) => (
-                                        <option key={cat.$id} value={cat.$id}>{cat.name}</option>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            <FormControl mb={4}>
-                                <FormLabel>Location</FormLabel>
-                                <Select
-                                    placeholder='Select Location'
-                                    value={productLocation}
-                                    onChange={(e) => setProductLocation(e.target.value)}
-                                >
-                                    {wareHouse.map((location) => (
-                                        <option key={location.$id} value={location.$id}>{location.name}</option>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            <FormControl mb={4}>
-                                <FormLabel>Quantity</FormLabel>
-                                <Input
-                                    placeholder='Quantity'
-                                    type='number'
-                                    value={productQuantity}
-                                    onChange={(e) => setProductQuantity(e.target.value)}
-                                />
-                            </FormControl>
-                            <FormControl mb={4}>
-                                <FormLabel>Price per piece</FormLabel>
-                                <Input
-                                    placeholder='Price'
-                                    type='number'
-                                    value={productPrice}
-                                    onChange={(e) => setProductPrice(e.target.value)}
+                                    placeholder="Enter category name"
+                                    type="text"
+                                    value={categoryName}
+                                    onChange={(e) => setCategoryName(e.target.value)}
                                 />
                             </FormControl>
                             <ModalFooter>
-                                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                <Button colorScheme="blue" mr={3} onClick={onClose}>
                                     Close
                                 </Button>
-                                <Button type='submit' variant='ghost'>
+                                <Button type="submit" variant="ghost">
                                     Create
                                 </Button>
                             </ModalFooter>
@@ -185,4 +108,4 @@ function AddProduct() {
     );
 }
 
-export default AddProduct;
+export default AddCategory;
